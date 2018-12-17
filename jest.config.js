926ac7ec860c9ff7acc45dacff5bb568d1bbcde0
Florian Rappl <florian.rappl@zeiss.com>
2018-12-17 22:59:46 +0100

    Updated test configuration

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,21 @@
 module.exports = {
   setupFiles: [
-    './test/unit/temp-polyfills.ts',
-    './test/unit/setup.ts'
+    './test/setup.ts',
   ],
   transform: {
     '^.+\\.(ts|tsx?)$': './node_modules/ts-jest/preprocessor.js',
   },
-  snapshotSerializers: ['enzyme-to-json/serializer'],
+  snapshotSerializers: [
+    'enzyme-to-json/serializer',
+  ],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  moduleFileExtensions: [
+    'ts',
+    'tsx',
+    'js',
+    'jsx',
+    'json',
+  ],
   globals: {
     'ts-jest': {
       diagnostics: false,
