e863b0d4bd6e808074b728d315bdcee98f9225dd
rad3k1 <uxxyyzz@gmail.com>
2020-01-20 13:21:59 +0100

    Override 'z-index'.

--- src/components/FileUploaderDetails/index.tsx
+++ src/components/FileUploaderDetails/index.tsx
@@ -64,7 +64,11 @@ const StyledUploaderHost = styled.div`
 `;
 
 const StyledDetailsHost = styled.div`
-  z-index: 10001;
+  // We override the 'z-index' of the styled component 'FixedContainer' of 'Blocker' (which is called by 'Modal').
+  // This fixes the problem of showing 'UploadProgressDetails' in front of 'parent' modal in IE.
+  & > div:first-child {
+    z-index: 10001;
+  }
 `;
 
 /**
